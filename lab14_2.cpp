9e76cf292b2405e812e86f66b1da3eea51ee8a2e
ํYuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2021-12-28 16:57:41 +0700

    Complete lap14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,25 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool photo[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i=0;i<N;i++){
+        cout << "|";
+        for(int j=0;j<M;j++){
+            if(photo[i][j]==0)cout << " ";
+            if(photo[i][j]==1)cout << "*";
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool photo[][M],int c,int a,int b){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(sqrt(pow(i-a,2) + pow(j-b,2))+1<= c){
+                photo[i][j]=1;
+            }
+        }
+    }
+}
